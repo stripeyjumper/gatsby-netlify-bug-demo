31e0ff9191d251a026d728eb538eba2af16ead2e
Andrew Bain <andrewb@rbvenergy.com>
2018-08-01 09:19:35 +0100

    Reproducing problem with gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
+const catPicture = '/static/cat.jpg'
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        allPageHeaders: [`Link: <${catPicture}>; rel=preload; as=image;`],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import cat from '../cat.jpg'
 import Layout from '../components/layout'
 
 const IndexPage = () => (
@@ -8,6 +8,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Here is a cat:</p>
+    <img alt="A cat" src={cat} />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
